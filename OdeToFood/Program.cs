ef361db6518c4869c4d5b89fb55c5a79d121e1ce
Orange Moose <erinkodara.verslui@gmail.com>
2022-10-20 12:50:54 +0300

    Configure middleware

--- OdeToFood/Program.cs
+++ OdeToFood/Program.cs
@@ -21,14 +21,14 @@ builder.Services.AddDbContext<OdeToFoodDbContext>(dbContextOptions =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// Middlewares to process HTTP requests, order matters
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-// Middleware, process HTTP requests, order matters
 
 app.UseHttpsRedirection();
 app.UseStaticFiles(); // Allows static files from wwwroot (default on), when used without arguments.
